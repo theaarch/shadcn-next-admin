ef1ff70a68680d82bcb7468f13ec560c60ed8e54
theaarch <theaarch64@gmail.com>
2026-01-05 16:37:30 +0800

    feat: Add interactive chart, data table, user navigation, and site header components while removing unused registration page layout

--- app/(auth)/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthLayout from "@/app/(auth)/layout";
-
-export default function Register() {
-    return (
-        <AuthLayout>
-            Register
-        </AuthLayout>
-    )
-}
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,10 +1,10 @@
-import { LoginForm } from "@/components/login-form"
+import { SignupForm } from "@/components/signup-form"
 
-export default function LoginPage() {
+export default function SignupPage() {
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <LoginForm />
+        <SignupForm />
       </div>
     </div>
   )
